a9d6c61c76f64c0ebf5d48480073a07af7bc6824
[Torri] <[rtorri.2022@gmail.com]>
2022-11-05 09:28:39 -0300

    tratando que ande

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ const path= require ("path");//une directorios
 
 app.use(express.static (path.resolve(__dirname, "public")))
 
-app.get("/", (_req, res) => {
-    res.sendFile(path.resolve(__dirname, "views/index.html"));
-});
+app.get("/",(_req, res) {
+        res.sendFile(path.resolve(__dirname, "views/index.html"));
+    });
 
 app.listen(3000,() => {
    console.log("Server Running on port", 3000);
