d51d1fa29ce0bec0f8e8ca5147faac924aba3c17
[Torri] <[rtorri.2022@gmail.com]>
2022-11-05 11:45:18 -0300

    corre happy

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
 const express = require('express');
 const app =  express();
-const path= require ('express path');//une directorios
+const path= require ('path');//une directorios
 
 app.use(express.static (path.resolve(__dirname, "public")))
 
-app.get('/',(req, res) => {
-        res.sendFile(path.resolve(__dirname, "views/index.html"));
+app.get('/',(req, res)=>{
+        res.sendFile(path.resolve  (__dirname, "views/index.html"));
     });
 
-app.listen(3000,() => {
-   console.log("Server Running on port", 3000);
-});
+app.listen(3000, () => console.log("Server Running on port",3000));
