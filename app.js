f28c01bb56c23ff50ef33b4db737c0effd2250d7
[Torri] <[rtorri.2022@gmail.com]>
2022-11-05 09:04:45 -0300

    creacion carpetas

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app =  express();
+const path= require ("path");//une directorios
+
+app.use(express.static (path.resolve(__dirname, "public")))
+
+app.get("/", (_req, res) => {
+    res.sendFile(path.resolve(__dirname, "views/index.html"));
+});
+
+app.listen(3000,() => {
+   console.log("Server Running on port", 3000);
+});
